ced15ceca12e4a16214b245fd410d3d8c0913aa4
alsm <al@hronrad.eu>
2018-11-01 21:29:52 +0000

    v5 client alpha

---
+++ packets/unsubscribe.go
@@ -0,0 +1,48 @@
+package packets
+
+import (
+	"bytes"
+	"io"
+	"net"
+)
+
+// Unsubscribe is the Variable Header definition for a Unsubscribe control packet
+type Unsubscribe struct {
+	PacketID   uint16
+	Properties *Properties
+	Topics     []string
+}
+
+// Unpack is the implementation of the interface required function for a packet
+func (u *Unsubscribe) Unpack(r *bytes.Buffer) error {
+	for {
+		t, err := readString(r)
+		if err != nil && err != io.EOF {
+			return err
+		}
+		if err == io.EOF {
+			break
+		}
+		u.Topics = append(u.Topics, t)
+	}
+
+	return nil
+}
+
+// Buffers is the implementation of the interface required function for a packet
+func (u *Unsubscribe) Buffers() net.Buffers {
+	var b bytes.Buffer
+	writeUint16(u.PacketID, &b)
+	for _, t := range u.Topics {
+		writeString(t, &b)
+	}
+	return net.Buffers{b.Bytes()}
+}
+
+// WriteTo is the implementation of the interface required function for a packet
+func (u *Unsubscribe) WriteTo(w io.Writer) (int64, error) {
+	cp := &ControlPacket{FixedHeader: FixedHeader{Type: UNSUBSCRIBE}}
+	cp.Content = u
+
+	return cp.WriteTo(w)
+}
---
+++ paho/cmd/stdoutsub/main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/eclipse/paho.golang/paho"
+)
+
+func main() {
+	server := flag.String("server", "127.0.0.1:1883", "The MQTT server to connect to ex: 127.0.0.1:1883")
+	topic := flag.String("topic", "#", "Topic to subscribe to")
+	qos := flag.Int("qos", 0, "The QoS to subscribe to messages at")
+	clientid := flag.String("clientid", "", "A clientid for the connection")
+	username := flag.String("username", "", "A username to authenticate to the MQTT server")
+	password := flag.String("password", "", "Password to match username")
+	flag.Parse()
+
+	paho.SetDebugLogger(log.New(os.Stderr, "SUB: ", log.LstdFlags))
+	msgChan := make(chan *paho.Publish)
+
+	conn, err := net.Dial("tcp", *server)
+	if err != nil {
+		log.Fatalf("Failed to connect to %s: %s", *server, err)
+	}
+
+	c := paho.NewClient()
+	c.Router = paho.NewSingleHandlerRouter(func(m *paho.Publish) {
+		msgChan <- m
+	})
+	c.Conn = conn
+
+	cp := &paho.Connect{
+		KeepAlive:  30,
+		ClientID:   *clientid,
+		CleanStart: true,
+		Username:   *username,
+		Password:   []byte(*password),
+	}
+
+	if *username != "" {
+		cp.UsernameFlag = true
+	}
+	if *password != "" {
+		cp.PasswordFlag = true
+	}
+
+	ca, err := c.Connect(context.Background(), cp)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	if ca.ReasonCode != 0 {
+		log.Fatalf("Failed to connect to %s : %d - %s", *server, ca.ReasonCode, ca.Properties.ReasonString)
+	}
+
+	fmt.Printf("Connected to %s\n", *server)
+
+	ic := make(chan os.Signal, 1)
+	signal.Notify(ic, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-ic
+		fmt.Println("signal received, exiting")
+		if c != nil {
+			d := &paho.Disconnect{ReasonCode: 0}
+			c.Disconnect(d)
+		}
+		os.Exit(0)
+	}()
+
+	sa, err := c.Subscribe(context.Background(), &paho.Subscribe{
+		Subscriptions: map[string]paho.SubscribeOptions{
+			*topic: paho.SubscribeOptions{QoS: byte(*qos)},
+		},
+	})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	if sa.Reasons[0] != byte(*qos) {
+		log.Fatalf("Failed to subscribe to %s : %d", *topic, sa.Reasons[0])
+	}
+	log.Printf("Subscribed to %s", *topic)
+
+	for m := range msgChan {
+		log.Println("Received message:", string(m.Payload))
+	}
+}
