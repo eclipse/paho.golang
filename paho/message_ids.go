54315cc60bea8c1b3e732c6fe98fb36ecec87b0a
Dimosthenis Kaponis <dkaponis@gmail.com>
2020-04-04 19:55:13 +0300

    Address review comments.

--- paho/message_ids.go
+++ paho/message_ids.go
@@ -41,8 +41,8 @@ type CPContext struct {
 // to messages with a messageid
 type MIDs struct {
 	sync.Mutex
-	index      map[uint16]*CPContext
-	lastIdUsed uint16
+	index  map[uint16]*CPContext
+	lastID uint16
 }
 
 // Request is the library provided MIDService's implementation of
@@ -51,40 +51,19 @@ func (m *MIDs) Request(c *CPContext) (uint16, error) {
 	m.Lock()
 	defer m.Unlock()
 
-	if m.lastIdUsed < 65535 {
-		m.lastIdUsed++
-	} else {
-		m.lastIdUsed = 1
-	}
+	for i, n := m.lastID, 0; n < 65535; i, n = i+1, n+1 {
+		if i == 0 {
+			i = 1
+		}
 
-	i := m.lastIdUsed
-	if _, ok := m.index[i]; !ok {
-		m.index[i] = c
-		return i, nil
-	}
-
-	i = m.getNextId(i)
-	if i > 0 {
-		m.index[i] = c
-		m.lastIdUsed = i
-		return i, nil
-	}
-
-	return 0, ErrNoMoreIDs
-}
-
-func (m *MIDs) getNextId(startFrom uint16) uint16 {
-	if startFrom > 65535 {
-		startFrom = 1
-	}
-
-	for i := startFrom; i < 65535; i++ {
 		if _, ok := m.index[i]; !ok {
-			return i
+			m.index[i] = c
+			m.lastID = 1
+			return i, nil
 		}
 	}
 
-	return 0
+	return 0, ErrNoMoreIDs
 }
 
 // Get is the library provided MIDService's implementation of
