1b134ad40805ae04532644fcca53e4a27cbf3b07
Bryan Kneis <b.kneis2011@gmail.com>
2022-08-15 15:59:29 +0100

    Remove UseClient

--- autopaho/auto.go
+++ autopaho/auto.go
@@ -326,13 +326,3 @@ func (c *ConnectionManager) Publish(ctx context.Context, p *paho.Publish) (*paho
 	}
 	return cli.Publish(ctx, p)
 }
-
-func (c *ConnectionManager) UseClient(fn func(*paho.Client) error) error {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-
-	if c.cli == nil {
-		return ConnectionDownError
-	}
-	return fn(c.cli)
-}
